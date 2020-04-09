c39bf4fa0bdb390b2b3e39b226644fe79a66a11c
Khush7068 <khush.7068@gmail.com>
2020-04-10 00:40:31 +0530

    Add custom SDK

--- app/src/main/java/soft/gen/rozarpay/MainActivity.java
+++ app/src/main/java/soft/gen/rozarpay/MainActivity.java
@@ -1,99 +1,110 @@
 package soft.gen.rozarpay;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import com.razorpay.Checkout;
+
 import com.razorpay.PaymentResultListener;
+import com.razorpay.Razorpay;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity implements PaymentResultListener {
-    private static final String TAG = MainActivity.class.getSimpleName();
+import java.security.SignatureException;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+public class MainActivity extends AppCompatActivity {
 
     EditText etx_amt;
     Button btn_pay;
     float amount;
+    private WebView webview;
+    Razorpay razorpay ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        razorpay    = new Razorpay(MainActivity.this , "rzp_test_FibWdhtgn2Hhf2");
+        webview = findViewById(R.id.payment_webview);
+// Hide the webview until the payment details are submitted
+        webview.setVisibility(View.GONE);
+        razorpay.setWebView(webview);
+        try {
+            JSONObject data = new JSONObject();
+            data.put("amount", 1000); // pass in currency subunits. For example, paise. Amount: 1000 equals ₹10
+            data.put("order_id", "order_DgZ26rHjbzLLY2");//sample order_id. Generate orders using Orders API
+            data.put("email", "gaurav.kumar@example.com");
+            data.put("contact", "9876543210");
+            JSONObject notes = new JSONObject();
+            notes.put("custom_field", "abc");
+            data.put("notes", notes);
+            data.put("method", "netbanking");
+            // Method specific fields
+            data.put("bank", "HDFC");
 
-        etx_amt = findViewById(R.id.amt);
-        btn_pay = findViewById(R.id.pay);
+            // Make webview visible before submitting payment details
+            webview.setVisibility(View.VISIBLE);
 
-        btn_pay.setOnClickListener(new View.OnClickListener() {
+            razorpay.submit(data, new PaymentResultListener() {
+                @Override
+                public void onPaymentSuccess(String razorpayPaymentId) {
+                    // Razorpay payment ID is passed here after a successful payment
+                }
+
+                @Override
+                public void onPaymentError(int code, String description) {
+                    // Error code and description is passed here
+                }
+            });
+
+        } catch (Exception e) {
+            Log.e("KS", "Error in submitting payment details", e);
+        }
+
+        startPayment();
+
+    }
+
+    private void startPayment() {
+
+        try {
+            JSONObject orderRequest = new JSONObject();
+            orderRequest.put("amount", 50000); // amount in the smallest currency unit
+            orderRequest.put("currency", "INR");
+            orderRequest.put("receipt", "order_rcptid_11");
+            orderRequest.put("payment_capture", false);
+
+//            Order order = razorpay.Orders.create(orderRequest);
+        } catch ( JSONException e) {
+            // Handle Exception
+            System.out.println(e.getMessage());
+        }
+
+        razorpay.getPaymentMethods(new Razorpay.PaymentMethodsCallback() {
             @Override
-            public void onClick(View view) {
-                amount = Integer.parseInt(etx_amt.getText().toString().trim());
-                amount = amount * 100;
-                startPayment(amount);
+            public void onPaymentMethodsReceived(String result) {
+                try {
+                    JSONObject paymentMethods = new JSONObject(result);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+
             }
         });
-
     }
 
-    private void startPayment(float amount) {
-        /**
-         * Instantiate Checkout
-         */
-        Checkout checkout = new Checkout();
-
-        /**
-         * Set your logo here
-         */
-//        checkout.setImage(R.drawable.rzp_logo);
-
-        /**
-         * Reference to current activity
-         */
-        final Activity activity = this;
-
-        /**
-         * Pass your payment options to the Razorpay Checkout as a JSONObject
-         */
-        
-        try {
-            JSONObject options = new JSONObject();
-
-            /**
-             * Merchant Name
-             * eg: ACME Corp || HasGeek etc.
-             */
-            
-            options.put("name", "Khush Sharma");
-            /**
-             * Amount is always passed in currency subunits
-             * Eg: "500" = INR 5.00
-             */
-            options.put("amount", amount);
-
-            checkout.open(activity, options);
-        } catch (Exception e) {
-            Log.e(TAG, "Error in starting Razorpay Checkout", e);
-        }
-    }
-
-    @Override
-    public void onPaymentSuccess(String razorpayPaymentID) {
-        
-        /**
-         * Add your logic here for a successful payment response
-         */
-        
-        Toast.makeText(this, "successful payment", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onPaymentError(int code, String response) {
-        /**
-         * Add your logic here for a failed payment response
-         */
-        Toast.makeText(this, "failed payment", Toast.LENGTH_SHORT).show();
-    }
 }
---
+++ app/src/main/java/soft/gen/rozarpay/PaymentActivity.java
@@ -0,0 +1,46 @@
+package soft.gen.rozarpay;
+
+import android.app.Activity;
+
+import android.content.Intent;
+import android.view.View;
+import android.os.Bundle;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import org.json.JSONObject;
+
+import com.razorpay.Razorpay;
+
+
+public class PaymentActivity extends Activity{
+    private Razorpay razorpay;
+    private Activity activity;
+    private WebView webview;
+    private ViewGroup outerBox;
+    private JSONObject payload;
+    private static final String TAG = PaymentActivity.class.getSimpleName();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        activity = this;
+        setContentView(R.layout.activity_layout_payment);
+        webview = (WebView) findViewById(R.id.payment_webview);
+        outerBox = (ViewGroup) findViewById(R.id.outerbox);
+
+        View button = findViewById(R.id.pay_btn);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(PaymentActivity.this, PaymentOptions.class);
+                startActivity(intent);
+
+            }
+
+        });
+    }
+}
